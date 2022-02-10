277d1305357acd511a26afa2f25a67e76e319ef7
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-10 15:54:00 -0300

    Update Bootstrapper.sh

--- Bootstrapper.sh
+++ Bootstrapper.sh
@@ -1,10 +1,11 @@
 if [[ -f "${HOME}/.local/share/grapejuice/user/wine-download/wine-tkg-staging-fsync-git-7.1.r2.gc437a01e/bin/wine" ]]; then
-
+echo ' '
 else
 echo 'Wine TKG Binary 7.1.r2 not founded'
 echo 'Install the newest mouse patch please'
 exit
 fi
+
 if [ -d "${HOME}/KRNL" ]; then
 echo 'Delete ${HOME}/KRNL To continue'
 exit
