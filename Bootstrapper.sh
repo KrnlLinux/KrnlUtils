6d3a2d081c6ae1fcf2c35a5c3db360fe99787c04
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-11 16:00:20 -0300

    Update Bootstrapper.sh

--- Bootstrapper.sh
+++ Bootstrapper.sh
@@ -1,21 +1,96 @@
+if [[ $(whoami) = 'root' ]]; 
+then
+echo 'Dont run this with root'
+exit
+fi
+
+distro=""
+
+# Distro Detection
+if [[ -f "/bin/emerge" ]];
+then
+$distro="gentoo"
+elif [[ -f "/bin/apt" ]];
+then
+$distro="debian"
+elif [[ -f "/bin/pacman" ]];
+then
+$distro="arch"
+else
+$distro="idk"
+fi
+
+if [[ $1 == "--help" ]];
+then
+echo 'Help:'
+echo '--download-krnl-dll : Downloads KRNL.DLL'
+echo '--grapejuice-install : Install grapejuice'
+echo '--krnl-download : Installs KRNL but not anything more'
+echo '-f : This was going to be called --run-download, the thing that this flag does is execute download.sh at the end of the installation'
+echo '--install-tkg : Installs WINE TKG Binary (mouse patch)'
+exit
+elif [[ $1 == "--download-krnl-dll" ]];
+then
+wget -q https://k-storage.com/bootstrapper/files/krnl.dll -O $HOME/krnl.dll
+exit
+elif [[ $1 == "--krnl-download" ]];
+then
+# KRNL Download flag
+export DOWNLOAD="$(curl https://pastebin.com/raw/gcH1DTED)"
+wget -q https://k-storage.com/bootstrapper/files/krnl.dll -O $HOME/krnl.dll
+wget -q $DOWNLOAD -O $HOME/krnl.exe
+exit
+elif [[ $1 == "--install-tkg" ]];
+then
+# Install WINE-TKG
+cd /tmp
+wget https://pastebin.com/raw/5SeVb005 -O install.py
+python3 install.py
+exit
+fi
+elif [[ $1 == "--grapejuice-install" ]];
+then
+
+if [[ $distro == "debian" ]];
+then
+# uvuntu insteletion
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y curl
+curl https://gitlab.com/brinkervii/grapejuice/-/raw/master/ci_scripts/signing_keys/public_key.gpg | sudo tee /usr/share/keyrings/grapejuice-archive-keyring.gpg > /dev/null
+sudo tee /etc/apt/sources.list.d/grapejuice.list <<< 'deb [signed-by=/usr/share/keyrings/grapejuice-archive-keyring.gpg] https://brinkervii.gitlab.io/grapejuice/repositories/debian/ universal main' > /dev/null
+sudo apt update -y
+sudo apt install -y grapejuice
+elif [[ $distro == "arch" ]];
+then
+# I use arch btw, installation
+cd $HOME
+sudo pacman -S base-devel
+git clone https://aur.archlinux.org/grapejuice-git.git
+cd grapejuice-git
+makepkg -si
+else
+# Gentoo Installation
+cd $HOME
+git clone https://gitlab.com/brinkervii/grapejuice.git
+cd grapejuice
+python3 ./install.py
+fi
+
+fi
+
 if [[ -f "${HOME}/.local/share/grapejuice/user/wine-download/wine-tkg-staging-fsync-git-7.1.r2.gc437a01e/bin/wine" ]]; 
 then
 echo ' '
 else
 echo 'Wine TKG Binary 7.1.r2 not founded'
-echo 'Install the newest mouse patch please'
+echo 'Install the newest mouse patch please, to do this is recommended to use the flag --install-tkg'
 exit
 fi
 
 if [[ -d "${HOME}/KRNL" ]]; 
 then
 echo 'Delete ${HOME}/KRNL To continue'
-exit
-fi
-
-if [[ $(whoami) = 'root' ]]; 
-then
-echo 'Dont run this with root'
 exit
 fi
 
