0dce397744cc316d221b7f196708f83736d30a0f
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-13 00:22:11 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -3,6 +3,7 @@ then
 cd /bin 
 wget https://raw.githubusercontent.com/SimpIyDeveIoper/KrnlUtils/main/KrnlUtils.py -O krnlutils > /dev/null 
 echo "#!$(which python3)" >> krnlutils
+sudo chmod +x /bin/krnlutils
 else
 echo 'This script must be runned with root'
 fi
