e3496220df453276e0e76543f823785dccdbbe4c
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-13 00:35:18 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,9 +1,10 @@
 if [[ "$(whoami)" = 'root' ]];
 then
-cd /bin 
-wget https://raw.githubusercontent.com/SimpIyDeveIoper/KrnlUtils/main/KrnlUtils.py -O krnlutils > /dev/null 
-python3path="$(which python3)"
-echo "#!${python3path}" >> krnlutils
+cd /etc 
+wget https://raw.githubusercontent.com/SimpIyDeveIoper/KrnlUtils/main/KrnlUtils.py -O krnlutils.py > /dev/null 
+cd /bin
+touch krnlutils
+echo 'python3 /etc/krnlutils.py' > krnlutils
 else
 echo 'This script must be runned with root'
 fi
