4b2e78def2c3aafd28c24855426e7f55fcb57257
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-12 23:31:21 -0300

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,8 @@
+if [[ '$(whoami)' = 'root' ]];
+then
+cd /bin 
+wget https://raw.githubusercontent.com/SimpIyDeveIoper/KrnlUtils/main/KrnlUtils.py -O krnlutils > /dev/null 
+echo '#!$(which python3)' >> krnlutils
+else
+echo 'This script must be runned with root'
+fi
