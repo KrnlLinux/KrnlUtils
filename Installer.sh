02b32c59d99a6012693941576647bc7014d13902
SimplyDeveloper <95656165+SimpIyDeveIoper@users.noreply.github.com>
2022-02-13 00:39:55 -0300

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -4,7 +4,7 @@ cd /etc
 wget https://raw.githubusercontent.com/SimpIyDeveIoper/KrnlUtils/main/KrnlUtils.py -O krnlutils.py > /dev/null 
 cd /bin
 touch krnlutils
-echo 'python3 /etc/krnlutils.py' > krnlutils
+echo 'python3 /etc/krnlutils.py $@' > krnlutils
 else
 echo 'This script must be runned with root'
 fi
